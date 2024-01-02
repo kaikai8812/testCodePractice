a9658251033c7fae9426ce824de004c5ffe5a686
kaikai <gamera1000x@gmail.com>
2024-01-02 15:38:01 +0900

    Initial Commit

---
+++ testCodePractice/testCodePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testCodePracticeApp.swift
+//  testCodePractice
+//
+//  Created by 青山凱 on 2024/01/02.
+//
+
+import SwiftUI
+
+@main
+struct testCodePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
